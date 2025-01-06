c924b25b138d8772edb232e76b3fbd72d053d3cf
boskadoff <mashagoncharenko@gmail.com>
2025-01-06 04:10:00 -0500

    new working site

--- elements/thingsDoneGallery/thingsDoneGallery.js
+++ elements/thingsDoneGallery/thingsDoneGallery.js
@@ -1,13 +1,16 @@
-$( function() {
-  var $grid = $('.grid').isotope({
-    itemSelector: 'article'
+jQuery(function() {
+  var $grid = jQuery('.grid').isotope({
+      itemSelector: 'article'
   });
-  // filter buttons
-  $('.filters-button-group').on( 'click', 'button', function() {
-    var filterValue = $( this ).attr('data-filter');
-    $grid.isotope({ filter: filterValue });
+  jQuery('.filters-button-group').on('click', 'button', function() {
+      var filterValue = jQuery(this).attr('data-filter');
+      $grid.isotope({ filter: filterValue });
   });
 });
+
+
 $(window).bind("load", function() {
   $('#all').click();
 });
+
+console.log('JavaScript file is successfully executed.');
