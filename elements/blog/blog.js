39eead4cdcd8260602c9dbaf267b30e7d56fb044
boskadoff <mashagoncharenko@gmail.com>
2025-01-07 02:07:31 -0500

    It works, just need to style it and add images, colors, themes, and portfolio data.
    STACKING BUG FOR FIRST VISIT
    The portfolio bar without cache does not load isotopes and jQuery in the right order. The stuff only loads in after the document refereshes. Casuing the wierd stacking bug in the first load. See thingsDoneGallery.html

--- elements/blog/blog.js
+++ elements/blog/blog.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const colors = ['#fffff', 'green', 'cyan', 'yellow', 'orange'];
+    const colors = ['#ffb6a3', '#f5d1c3', '#aab8bb', '#5f9595', '#f0bc68'];
     const cards = document.querySelectorAll('.card');
 
     cards.forEach(card => {
